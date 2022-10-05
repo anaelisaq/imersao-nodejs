d4ccbddf5ebaab09a39b215ce2dfd883dded352a
anaelisaq <anaelisa.abs@gmail.com>
2022-10-05 10:45:26 -0300

    add: crud made w/ tdd and command line - cli

---
+++ modulo4-crud/database.js
@@ -0,0 +1,85 @@
+//usar orientação a objeto
+const {
+    readFileSync, writeFileSync
+} = require("fs")
+
+// const {
+//     promisify
+// } = require("util")
+
+//já existe dentro do módulo fs funções sync
+// const readFileAsync = promisify(readFile)
+// const writeFileAsync = promisify(writeFile)
+
+//outra forma de obter dados do json
+//const dadosJson = require("./herois.json")
+
+class Database {
+    constructor(){
+        this.NOME_ARQUIVO = "herois.json"
+    }
+    async obterDadosArquivo(){
+        const arquivo = await readFileSync(this.NOME_ARQUIVO, "utf8")
+        return JSON.parse(arquivo.toString())
+    }
+
+    async escreverArquivo(dados){
+        await writeFileSync(this.NOME_ARQUIVO, JSON.stringify(dados))
+        return true
+    }
+
+    async cadastrar(heroi){
+        const dados = await this.obterDadosArquivo()
+        const id = heroi.id <= 2 ? heroi.id : Date.now() //simular que tá criando id
+        const heroiComId = {
+            id,
+            ...heroi
+        } //concatenar os objetos para retornar em um só
+
+        const dadosFinal = [
+            ...dados,
+            heroiComId
+        ] //concatenar dois objetos num array
+        const resultado = await this.escreverArquivo(dadosFinal)
+        return resultado
+    }
+
+    async listar(id){
+        const dados = await this.obterDadosArquivo()
+        const dadosFiltrados = dados.filter(item => (id ? (item.id === id) : true))
+        return dadosFiltrados
+    }
+
+    async remover(id){
+        if(!id) {
+            return await this.escreverArquivo([])
+        }
+        const dados = await this.obterDadosArquivo()
+        const indice = dados.findIndex(item => item.id === parseInt(id))
+        if(indice === -1){
+            throw Error("o usuario informado não existe")
+        }
+        dados.splice(indice, 1)
+        return await this.escreverArquivo(dados)
+    }
+
+    async atualizar(id, modificacoes){
+        const dados = await this.obterDadosArquivo()
+        const indice = dados.findIndex(item => item.id === parseInt(id))
+        if(indice === -1){
+            throw Error("O heroi informado não existe")
+        }
+        const atual = dados[indice]
+        const objetoAtualizar = {
+            ...atual,
+            ...modificacoes
+        }
+        dados.splice(indice, 1)
+        return await this.escreverArquivo([
+            ...dados,
+            objetoAtualizar
+        ])
+    }
+}
+
+module.exports = new Database()
---
+++ modulo4-crud/test.js
@@ -0,0 +1,59 @@
+const {
+    deepEqual
+} = require("assert")
+
+const database = require("./database")
+
+const DEFAULT_ITEM_CADASTRAR = { 
+    nome: "Flash",
+    poder: "Speed", 
+    id: 1
+}
+
+const DEFAULT_ITEM_ATUALIZAR = {
+    nome: "Lanterna Verde",
+    poder: "Energia do anel",
+    id: 2
+}
+
+describe("Suite de manipulação de Herois", () => {
+    before(async () => {
+        await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+        await database.cadastrar(DEFAULT_ITEM_ATUALIZAR)
+    }) //para não deixar a lista vazia quando for cadastrar
+
+    it("deve pesquisar um heroi usando arquivos", async () => {
+        const expected = DEFAULT_ITEM_CADASTRAR
+        const [resultado] = await database.listar(expected.id)
+        
+        deepEqual(resultado, expected)
+    })
+
+    it("deve cadastrar um heroi usando arquivos", async () => {
+        const expected = DEFAULT_ITEM_CADASTRAR
+        const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+        const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
+        deepEqual(actual, expected)
+    })
+
+    it("deve atualizar um heroi pelo id", async () => {
+        const expected = {
+            ...DEFAULT_ITEM_ATUALIZAR,
+            nome: "Batman",
+            poder: "Dinheiro"
+        }
+        const novoDado = {
+            nome: "Batman",
+            poder: "Dinheiro"
+        }
+        await database.atualizar(DEFAULT_ITEM_ATUALIZAR.id, novoDado)
+        const [resultado] = await database.listar(DEFAULT_ITEM_ATUALIZAR.id)
+        deepEqual(resultado, expected)
+    })
+
+    it("deve remover um heroi por id", async () => {
+        const expected = true
+        const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id)
+        deepEqual(resultado, expected)
+    })
+})
